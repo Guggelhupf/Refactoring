9562295c16131eaf5bc06ee77299a895b972fe31
Lukas Schreck <LukasSchreck@web.de>
2014-05-25 12:22:33 +0200

    Refactoring 01 amountfor-method

--- Customer.java
+++ Customer.java
@@ -25,22 +25,10 @@ class Customer {
         while (enum_rentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                	thisAmount += 2;
-                	if (each.getDaysRented() > 2)
-                    	thisAmount += (each.getDaysRented() - 2) * 1.5;
-                	break;
-                case Movie.NEW_RELEASE:
-                	thisAmount += each.getDaysRented() * 3;
-                	break;
-                case Movie.CHILDRENS:
-                	thisAmount += 1.5;
-                	if (each.getDaysRented() > 3)
-                    	thisAmount += (each.getDaysRented() - 3) * 1.5;
-                	break;
-            }
+            
+            // Refactoring 01
+            thisAmount = amountFor(each); 
+
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -56,5 +44,27 @@ class Customer {
         return result;
     }
     
+
+// Refactoring 01
+private double amountFor(Rental each) { 
+    double thisAmount = 0; 
+    switch (each.getMovie().getPriceCode()) { 
+        case Movie.REGULAR: 
+            thisAmount += 2; 
+            if (each.getDaysRented() > 2) 
+                thisAmount += (each.getDaysRented() - 2) * 1.5; 
+            break; 
+        case Movie.NEW_RELEASE: 
+            thisAmount += each.getDaysRented() * 3; 
+            break; 
+        case Movie.CHILDRENS: 
+            thisAmount += 1.5; 
+            if (each.getDaysRented() > 3) 
+                thisAmount += (each.getDaysRented() - 3) * 1.5; 
+            break; 
+ 
+    } 
+    return thisAmount; 
 }
-    
+  
+}
